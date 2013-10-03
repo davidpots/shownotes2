3d28af41d9922a19ade851415e017e215cb38db7
David Potsiadlo <davidpots@gmail.com>
2013-10-03 09:11:05 -0400

    First commit. DBs setup.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shownotes::Application.config.secret_token = 'c4625decdb630be3953b7818105c9977a2d0b09a1a9f09afe3a9fa24e8e45089259c304779d31a0aa4e5dc592caf744e5d7a09e38617e66a277d1f02082b31b1'
