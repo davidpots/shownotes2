a49e793c810ddea291a60605304aec7aca5436ed
David Potsiadlo <davidpots@gmail.com>
2013-10-30 09:54:23 -0400

    Mucho progress at beginning to implement responsiveness into the actual app. Still some bugs, but mostly working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,40 +14,76 @@
 //= require jquery_ujs
 //= require_tree .
 
+
+
+// Add/remove a class based on size of window. Inspired by https://gist.github.com/RyanBrackett/6107983
+
+          // Change width value on page load
+          $(document).ready(function(){
+              responsive_resize();
+          });
+
+          // Change width value on user resize, after DOM
+          $(window).resize(function(){
+               responsive_resize();
+          });
+
+          function responsive_resize(){
+            var current_width = $(window).width();
+            if(current_width < 1027){
+              $('html').addClass("mobile").removeClass("desktop");
+            } 
+            if(current_width > 1027){
+              $('html').addClass("desktop").removeClass("mobile");
+            }
+          }
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function(){
+  
+  // If user clicks on a search result...
   $('body').on('click', 'a.feed', function(){
     $('a.feed').removeClass('active');
+    $('body').addClass('showing-details');
     $(this).addClass('active');
     var url_rss = $(this).data('url-rss');
     var url_itunes = $(this).data('url-itunes');
     $('#feed_select').find('#url_rss').val(url_rss);
     $('#feed_select').find('#url_itunes').val(url_itunes);
+
+    if ($('html').hasClass('mobile')) {
+      $('#podcast_wrap_inner').css('opacity','0.0');
+    } else {
+      $('#podcast_wrap_inner').css('opacity','0.3');
+    }
+
     $('#feed_select').submit();
-    $('#podcast_wrap_inner').css('opacity','0.3');
     $('.spinner').show();
-  });
-
-
-  // RESPONSIVE STUFF
-
-  $('body').click(function(){
-    $('body').addClass('show-details');
-  });
-
-  $('.back-to a').click(function(){
-    $('body').removeClass('show-details');
-    $('.side ul li a').removeClass('active');
     return false;
   });
 
-  $('.side ul li a').click(function(){
-    $('.side ul li a').removeClass('active');
-    $(this).addClass('active');
+
+  // If in mobile view, user clicks BACK TO PODCASTS link...
+  $('body').on('click', '.back-to a', function(){
+    $('a.feed').removeClass('active');
+    $('body').removeClass('showing-details');
+    return false;
   });
 
 });
 
 $(window).bind("load", function() {
-  $('.feeds a.feed').first().click();
+  // If a desktop view, then auto-click the first search result
+  if ($('html').hasClass('desktop')) {
+    $('.feeds a.feed').first().click();
+  }
   $('.feeds-wrap-inner').css('opacity','1');
 });
