06dda72ce979122799f190921db1a05bd6418205
David Potsiadlo <davidpots@gmail.com>
2013-11-05 07:45:21 -0500

    Updates the broken spinner, finally

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -66,12 +66,6 @@ $(document).ready(function(){
     $('#feed_select').find('#url_rss').val(url_rss);
     $('#feed_select').find('#url_itunes').val(url_itunes);
 
-    if ($('html').hasClass('mobile')) {
-      $('#podcast_wrap_inner').css('opacity','0.0');
-    } else {
-      $('#podcast_wrap_inner').css('opacity','0.3');
-    }
-
     $('#feed_select').submit();
     $('.spinner').show();
     return false;
@@ -101,7 +95,6 @@ $(document).ready(function(){
           if ($('html').hasClass('desktop')) {
             $('.feeds a.feed').first().click();
           }
-          $('.feeds-wrap-inner').css('opacity','1');
         });
 
 
